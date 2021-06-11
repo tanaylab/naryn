649f355dad9a12a4eda6c25897e8676cd1abd51c
aviezerl <aviezer.lifshitz@weizmann.ac.il>
2021-06-11 11:52:31 +0300

    use test_path for initialization of testdb

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,10 +1,12 @@
 # Note: we should change this with download.file from a public link
-if (dir.exists("../testdb")) {
-    system("rm -rf ../testdb")
+library(dplyr, warn.conflicts = FALSE)
+library(glue, warn.conflicts = FALSE)
+
+testdb_dir <- test_path("../testdb")
+if (dir.exists(testdb_dir)) {
+    system(glue("rm -rf {testdb_dir}"))
 }
-system("cp -rf /net/mraid14/export/tgdata/db/tgdb/emr/naryn_testdb ../testdb")
+system(glue("cp -rf /net/mraid14/export/tgdata/db/tgdb/emr/naryn_testdb {testdb_dir}"))
 
-library(dplyr)
-
-emr_db.init("../testdb", "../testdb/utest")
+emr_db.init(testdb_dir, file.path(testdb_dir, "utest"))
 emr_db.reload()
