34b6f81fd498779c84245935c20d87ba59b82dff
aviezerl <aviezer.lifshitz@weizmann.ac.il>
2021-06-11 12:00:26 +0300

    cosmetics

--- tests/testthat/test-track.info.R
+++ tests/testthat/test-track.info.R
@@ -7,21 +7,25 @@ test_that("emr_track.info works dense track", {
     track_info <- emr_track.info("track4")
     track_info$path <- NULL
     expect_equal(
-        track_info, 
-        list(type = "dense", data.type = "float", categorical = FALSE,
+        track_info,
+        list(
+            type = "dense", data.type = "float", categorical = FALSE,
             num.vals = 500L, num.unique.vals = 401L, min.val = 1, max.val = 997,
-            min.id = 2L, max.id = 998L, min.time = 6L, max.time = 9996L)    
-    )    
+            min.id = 2L, max.id = 998L, min.time = 6L, max.time = 9996L
+        )
+    )
 })
 
 test_that("emr_track.info works sparse track", {
     track_info <- emr_track.info("track1_sparse")
     track_info$path <- NULL
     expect_equal(
-        track_info, 
-        list(type = "sparse", data.type = "float", categorical = FALSE,
+        track_info,
+        list(
+            type = "sparse", data.type = "float", categorical = FALSE,
             num.vals = 500001L, num.unique.vals = 1000L, min.val = 0,
             max.val = 999, min.id = 0L, max.id = 2510L, min.time = 0L,
-            max.time = 9999L) 
+            max.time = 9999L
+        )
     )
 })
