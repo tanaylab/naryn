786fe2579e3db28a84f4284e68f85c6b7b0eb50e
aviezerl <aviezer.lifshitz@weizmann.ac.il>
2021-08-16 11:39:44 +0300

    separated test_db and regression from setup.R

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -2,14 +2,7 @@
 library(dplyr, warn.conflicts = FALSE)
 library(glue, warn.conflicts = FALSE)
 
-testdb_dir <- test_path("../testdb")
-if (dir.exists(testdb_dir)) {
-    system(glue("rm -rf {testdb_dir}"))
-}
-system(glue("cp -rf /net/mraid14/export/tgdata/db/tgdb/emr/naryn_testdb {testdb_dir}"))
-
-emr_db.init(testdb_dir, file.path(testdb_dir, "utest"))
-emr_db.reload()
+load_test_db()
 
 # Clean examples db
 withr::defer(
@@ -19,23 +12,3 @@ withr::defer(
     },
     teardown_env()
 )
-
-#' Tests if an object was changed since the last run.
-#' If an rds file named \code{snapshot_dir/id.rds} exists its contents are compared with \{obj},
-#' otherwise the file is created.
-#'
-#' @param obj an R object
-#' @param id unique test id.
-#' @param snapshot_dir directory with rds file containing snapshot of previous versions
-expect_regression <- function(obj, id, snapshot_dir = "/net/mraid14/export/tgdata/db/tgdb/emr/naryn_snapshot") {
-    regression_file <- file.path(snapshot_dir, glue("{id}.rds"))
-
-    if (!file.exists(regression_file)) {
-        readr::write_rds(obj, regression_file)
-        system(glue("chmod a-w {regression_file}"))
-    }
-
-    # We need testthat to always find the `expect` statement (otherwise - the test would be skipped)
-    old <- readr::read_rds(regression_file)
-    expect_identical(old, obj)
-}
