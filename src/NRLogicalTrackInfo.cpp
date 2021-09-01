fa5bc3158888c35ca851061eae6c9090549bf3ac
Ben <gilatb@n100.mcl4.weizmann.ac.il>
2021-09-01 16:41:03 +0300

    all info is returned except num of unique vals, tbc

--- src/NRLogicalTrackInfo.cpp
+++ src/NRLogicalTrackInfo.cpp
@@ -2,6 +2,7 @@
 
 #include "naryn.h"
 #include "NRTrackExpressionScanner.h"
+#include "EMRPoint.h"
 
 #include <R.h>
 #include <Rinternals.h>
@@ -10,55 +11,40 @@
 #undef length
 #endif
 
-struct IntervalSummary {
-	double num_bins;
-	double num_non_nan_bins;
-	double total;
+struct LogicalTrackInfo {
+    double num_vals;
 	double minval;
 	double maxval;
-	double mean_square_sum;
+    unsigned maxid;
+    unsigned minid;
+    unsigned maxtime;
+    unsigned mintime;
 
-	double get_mean() const { return total / num_non_nan_bins; }
+    void _update_id_time_info(EMRPoint v) {
+        minid = min(minid, v.id);
+        maxid = max(maxid, v.id);
+        mintime = min(mintime, v.timestamp.hour());
+        maxtime = max(maxtime, v.timestamp.hour());
+    }
 
-	double get_stdev() const {
-		double mean = get_mean();
-		// we are calaculating unbiased standard deviation:
-		// sqrt(sum((x-mean)^2) / (N-1)) = sqrt(sum(x^2)/(N-1) - N*(mean^2)/(N-1))
-		return sqrt(mean_square_sum / (num_non_nan_bins - 1) - (mean * mean) * (num_non_nan_bins / (num_non_nan_bins - 1)));
-	}
+    void _update_val_info(double v) {
+        num_vals++;
+        minval = min(minval, v);
+	    maxval = max(maxval, v);
+    }
 
-	void update(double v) {
-		++num_bins;
-		if (!std::isnan(v)) {
-			num_non_nan_bins++;
-			total += v;
-			minval = min(minval, v);
-			maxval = max(maxval, v);
-			mean_square_sum += v * v;
-		}
-	}
 
-	void merge(const IntervalSummary &obj) {
-		num_bins += obj.num_bins;
-		num_non_nan_bins += obj.num_non_nan_bins;
-		total += obj.total;
-		minval = min(minval, obj.minval);
-		maxval = max(maxval, obj.maxval);
-		mean_square_sum += obj.mean_square_sum;
-	}
-
-	IntervalSummary() :
-		num_bins(0),
-		num_non_nan_bins(0),
-		total(0),
-		minval(numeric_limits<double>::max()),
-		maxval(-numeric_limits<double>::max()),
-		mean_square_sum(0) {}
+	LogicalTrackInfo() :
+	 	minval(numeric_limits<double>::max()),
+	 	maxval(-numeric_limits<double>::max()),
+     	minid(numeric_limits<unsigned>::max()),
+     	maxid(0),
+     	mintime(numeric_limits<unsigned>::max()),
+     	maxtime(0),
+	 	num_vals(0) {}
 };
 
-enum IntervalSummaryCols { TOTAL_BINS, TOTAL_NAN_BINS, MIN, MAX, SUM, MEAN, STDEV, NUM_COLS };
 
-static const char *IntervalSummaryColNames[NUM_COLS] = { "Total values", "NaN values", "Min", "Max", "Sum", "Mean", "Std dev" };
 
 extern "C" {
 
@@ -66,34 +52,74 @@ SEXP emr_logical_track_user_info(SEXP _expr, SEXP _stime, SEXP _etime, SEXP _ite
 {
 	try {
         Naryn naryn(_envir);
+        
+        enum { PATH, TYPE, DATA_TYPE, CATEGORICAL, NUM_VALS, NUM_UNIQUE_VALS, MIN_VAL, MAX_VAL, MIN_ID, MAX_ID, MIN_TIME, MAX_TIME, NUM_COLS };
+        const char *COL_NAMES[NUM_COLS] = { "path", "type", "data.type", "categorical", "num.vals", "num.unique.vals", "min.val", "max.val", "min.id", "max.id", "min.time", "max.time" };
 
-		if (!isString(_expr) || Rf_length(_expr) != 1)
-			verror("The value of 'expr' parameter must be a string");
 
-        IntervalSummary summary;
+        LogicalTrackInfo summary;
 		NRTrackExprScanner scanner;
+        const char *trackname = CHAR(STRING_ELT(_expr, 0));
+        EMRTrack *track = g_db->track(trackname);
+        const EMRDb::TrackInfo *track_info = g_db->track_info(trackname);
 
-		for (scanner.begin(_expr, NRTrackExprScanner::REAL_T, _stime, _etime, _iterator_policy, _keepref, _filter); !scanner.isend(); scanner.next())
-            summary.update(scanner.real());
+		for (scanner.begin(_expr, NRTrackExprScanner::REAL_T, _stime, _etime, _iterator_policy, _keepref, _filter); !scanner.isend(); scanner.next()) {
+            summary._update_id_time_info(scanner.point());
+            summary._update_val_info(scanner.real());
+        }
+            
 
-        SEXP answer;
-        SEXP colnames;
+		SEXP answer;
+        // SEXP names, rtype, rdata_type, rcategorical, rnum_vals, rnum_unique_vals, rmin_val, rmax_val, rmin_id, rmax_id, rmin_time, rmax_time;
+        SEXP names, rtype, rdata_type, rcategorical, rnum_vals, rmin_id, rmax_id, rmin_time, rmax_time,  rmin_val, rmax_val;
 
-        rprotect(answer = RSaneAllocVector(REALSXP, NUM_COLS));
-        rprotect(colnames = RSaneAllocVector(STRSXP, NUM_COLS));
 
-        REAL(answer)[TOTAL_BINS] = summary.num_bins;
-        REAL(answer)[TOTAL_NAN_BINS] = summary.num_bins - summary.num_non_nan_bins;
-        REAL(answer)[MIN] = summary.num_non_nan_bins ? summary.minval : numeric_limits<double>::quiet_NaN();
-        REAL(answer)[MAX] = summary.num_non_nan_bins ? summary.maxval : numeric_limits<double>::quiet_NaN();
-        REAL(answer)[SUM] = summary.num_non_nan_bins ? summary.total : numeric_limits<double>::quiet_NaN();
-        REAL(answer)[MEAN] = summary.num_non_nan_bins ? summary.get_mean() : numeric_limits<double>::quiet_NaN();
-        REAL(answer)[STDEV] = summary.num_non_nan_bins > 1 ? summary.get_stdev() : numeric_limits<double>::quiet_NaN();
+		rprotect(answer = RSaneAllocVector(VECSXP, NUM_COLS));
+        rprotect(names = RSaneAllocVector(STRSXP, NUM_COLS));
+        // // rprotect(rpath = RSaneAllocVector(STRSXP, 1));
+        rprotect(rtype = RSaneAllocVector(STRSXP, 1));
+        rprotect(rdata_type = RSaneAllocVector(STRSXP, 1));
+        rprotect(rcategorical = RSaneAllocVector(LGLSXP, 1));
+        rprotect(rnum_vals = RSaneAllocVector(INTSXP, 1));
+        // rprotect(rnum_unique_vals = RSaneAllocVector(INTSXP, 1));
+        rprotect(rmin_val = RSaneAllocVector(REALSXP, 1));
+        rprotect(rmax_val = RSaneAllocVector(REALSXP, 1));
+        rprotect(rmin_id = RSaneAllocVector(INTSXP, 1));
+        rprotect(rmax_id = RSaneAllocVector(INTSXP, 1));
+        rprotect(rmin_time = RSaneAllocVector(INTSXP, 1));
+        rprotect(rmax_time = RSaneAllocVector(INTSXP, 1));
+
+		// // SET_STRING_ELT(rpath, 0, mkChar(track_info->filename.c_str()));
+        SET_STRING_ELT(rtype, 0, mkChar(EMRTrack::TRACK_TYPE_NAMES[track->track_type()]));
+        SET_STRING_ELT(rdata_type, 0, mkChar(EMRTrack::DATA_TYPE_NAMES[track->data_type()]));
+        LOGICAL(rcategorical)[0] = track->is_categorical();
+        INTEGER(rnum_vals)[0] = summary.num_vals;
+        // INTEGER(rnum_unique_vals)[0] = track->unique_size();
+        REAL(rmin_val)[0] = summary.minval ? summary.minval : numeric_limits<double>::quiet_NaN();
+        REAL(rmax_val)[0] = summary.maxval ? summary.maxval : numeric_limits<double>::quiet_NaN();
+        INTEGER(rmin_id)[0] = summary.minid ? summary.minid : NA_INTEGER;
+        INTEGER(rmax_id)[0] = summary.maxid ? summary.maxid : NA_INTEGER;
+        INTEGER(rmin_time)[0] = summary.mintime ? summary.mintime : NA_INTEGER;
+        INTEGER(rmax_time)[0] = summary.maxtime ? summary.maxtime : NA_INTEGER;
+
 
         for (int i = 0; i < NUM_COLS; i++)
-            SET_STRING_ELT(colnames, i, mkChar(IntervalSummaryColNames[i]));
+            SET_STRING_ELT(names, i, mkChar(COL_NAMES[i]));
 
-        setAttrib(answer, R_NamesSymbol, colnames);
+		SET_VECTOR_ELT(answer, CATEGORICAL, rcategorical);
+        // // SET_VECTOR_ELT(answer, PATH, rpath);
+        SET_VECTOR_ELT(answer, TYPE, rtype);
+        SET_VECTOR_ELT(answer, DATA_TYPE, rdata_type);
+        SET_VECTOR_ELT(answer, NUM_VALS, rnum_vals);
+        // SET_VECTOR_ELT(answer, NUM_UNIQUE_VALS, rnum_unique_vals);
+        SET_VECTOR_ELT(answer, MIN_VAL, rmin_val);
+        SET_VECTOR_ELT(answer, MAX_VAL, rmax_val);
+        SET_VECTOR_ELT(answer, MIN_ID, rmin_id);
+        SET_VECTOR_ELT(answer, MAX_ID, rmax_id);
+        SET_VECTOR_ELT(answer, MIN_TIME, rmin_time);
+        SET_VECTOR_ELT(answer, MAX_TIME, rmax_time);
+
+        setAttrib(answer, R_NamesSymbol, names);
 
         rreturn(answer);
 	} catch (TGLException &e) {
