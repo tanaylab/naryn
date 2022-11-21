11d9dea2c088ee1ddee5932dd27b43666964ebc5
Aviezer Lifshitz <aviezer.lifshitz@weizmann.ac.il>
2022-11-21 11:41:40 +0200

    removed redundant examples

--- R/iterator.R
+++ R/iterator.R
@@ -71,11 +71,6 @@ iterator_by_period <- function(stime, etime, n, period) {
 #' iter <- emr_monthly_iterator(emr_date2time(1, 1, 2002), n = 15)
 #' emr_extract("dense_track", iterator = iter, stime = 1, etime = 3)
 #'
-#' iter <- emr_yearly_iterator(emr_date2time(1, 1, 2002), emr_date2time(1, 1, 2017))
-#' emr_extract("dense_track", iterator = iter, stime = 1, etime = 3)
-#'
-#' iter <- emr_yearly_iterator(emr_date2time(1, 1, 2002), n = 4)
-#' emr_extract("dense_track", iterator = iter, stime = 1, etime = 3)
 #'
 #' @export
 emr_monthly_iterator <- function(stime, etime = NULL, n = NULL) {
